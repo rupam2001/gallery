7c604183db35ad0b7a8fae84f75c3482740c950f
rupam2001 <34038003+rupam2001@users.noreply.github.com>
2020-04-18 18:44:20 +0530

    cloudinary added
    new module: Cloudinary

--- backend/server.js
+++ backend/server.js
@@ -3,32 +3,62 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-require("dotenv/config");
+var cloudinary = require('cloudinary');//////////////////
 
+require("dotenv/config");
 app.use(bodyParser.json());
 app.use(cors());
 
 //routes
 app.get("/", (req, res) => {
-  res.send(`<h1>Running</h1>`);
+    res.send(`<h1>Running</h1>`);
 });
 
 //importing routes
 const chat = require("./routes/chat");
 app.use("/chat", chat);
 
+
+mongoose.connect("mongodb+srv://art_gallery:art_gallery@aurinassam@cluster0-edso8.mongodb.net/test?retryWrites=true&w=majority"
+    , { useNewUrlParser: true, useUnifiedTopology: true })
+const connecton = mongoose.connection;
+connecton.once('open', () => {
+    console.log('Connected to DB');
+});
+const Schema = mongoose.Schema;
+const ContentsSchema = new Schema({
+    imageLink: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    }
+});
+const Contents = mongoose.model('contentsBase', ContentsSchema);
+///////////////////////////////////////////////////////////////////////////////
+cloudinary.config({
+    cloud_name: 'dyezvdvzx',
+    api_key: 845566372755842,
+    api_secret: 'vxkhuC2p0bSejkAIQitT8V_RyhA'
+});
+app.get('/', (req, res) => {
+    Contents.find()
+        .then(contents => {
+            res.json({ contents: contents });
+        })
+        .catch(err => { res.status(500).json({}); });
+});
+
 //notification
 const notification = require("./routes/notification");
 app.use("/notify", notification);
 
-//connect to Db
-//mongoose.connect(
-//  process.env.DB_CONNECTION,
-//  { useNewUrlParser: true, useUnifiedTopology: true },
-//  () => {
-//    console.log("Connected to DB");
-//  }
-//);
+
 
 const port = process.env.PORT || 3300;
 
